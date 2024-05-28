2d895e933e724369a6a1b45aea2072a62aa04ba2
Jed Staley <JNStaley1@gmail.com>
2024-05-28 16:01:25 -0400

    Got hangman working

---
+++ scratch.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const readlineSync = require('readline-sync');
+
+console.log('hello');
+
+const gameChoice = readlineSync.question(`What game would you like to play? Enter a number between 1 and 3: `);
+
+console.log('world');
+console.log('You chose:', gameChoice);
